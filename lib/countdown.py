4db729f1e9a708a5b2340d7cd860f2435e9d6fb2
hadil <hihijazi@gmail.com>
2024-01-10 20:26:45 -0500

    save changes

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,22 @@
 # your code goes here!
+# import ipdb
+import time
+
+
+def countdown(int):
+    while int >= 1:
+        print(f"{int} SECOND(S)!")
+        int -= 1
+        # countdown_with_sleep(1)
+    print('HAPPY NEW YEAR!')
+
+countdown(4)
+
+
+def countdown_with_sleep(int):
+    while int >= 1:
+        print(f"{int} SECOND(S)!")
+        int -= 1
+        time.sleep(1)
+    print('HAPPY NEW YEAR!')
+countdown_with_sleep(5)
